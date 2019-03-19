89d79025609d5aaac0bf4f2ba433d3e9c8fc90c2
ons <adrian.harris@ons.gov.uk>
2019-03-19 11:30:13 +0000

    Algorithmia initial commit

---
+++ src/main/java/algorithmia/ValidationValuePresent/ValidationValuePresent.java
@@ -0,0 +1,13 @@
+package algorithmia.ValidationValuePresent;
+
+import com.algorithmia.*;
+import com.algorithmia.algo.*;
+import com.algorithmia.data.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class ValidationValuePresent {
+    public String apply(String s) throws Exception {
+        return "Hello " + s;
+    }
+}
